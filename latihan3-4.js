0e73a6d7494f9db117c0ec708e79297f5fe44006
dulabdul <dul.abdoel21@gmail.com>
2021-09-03 23:31:30 +0700

    latihan 3&4 JS Dom Selection

---
+++ latihan3-4.js
@@ -0,0 +1,39 @@
+// document.getElementById => Element
+
+// const test = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = "blue";
+
+
+// document.getElementsByTagName => HTMLCollection
+
+// const test = document.getElementsByTagName('p');
+// for (let i = 0; i < test.length; i++) {
+//     test[i].style.color = 'red';
+// }
+
+// document.getElementsByClassName => HTMLCollection
+
+// const test = document.getElementsByClassName('list')
+// for (let i = 0; i < test.length; i++) {
+//     test[i].style.backgroundColor = 'blue';    
+// }
+
+// document.querySelector() => Element
+
+// const test = document.querySelector('.list1')
+// test.style.backgroundColor = 'blue';
+// test.innerHTML = "ini adalah href dengan Javascript"
+
+// document.querySelectorAll() => nodeList
+
+// const test = document.querySelectorAll('p');
+// for( let i = 0; i < test.length; i++){
+//     test[i].style.color = 'blue';
+// }
+
+// mengubah node root
+
+// const sectionB = document.getElementById('b');
+// const test = sectionB.querySelector('li');
+// test.style.backgroundColor = 'blue';
